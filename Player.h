15fb27c844c183e079638d027bec5dc14980cc04
春拓 藤巻 <syunta_0420@icloud.com>
2023-09-05 11:29:40 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.h
@@ -0,0 +1,29 @@
+#include "Novice.h"
+
+class Player {
+public:
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// ©ƒLƒƒƒ‰
+	/// XV
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
+
+private:
+	
+	
+
+};
+
+Player::Player() {}
+
+Player::~Player() {}
