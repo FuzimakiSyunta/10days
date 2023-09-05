2ddcdb912ea74d2f234945d2f04ef73215c16789
HAMA_ <HAMA_@MSI>
2023-09-05 12:12:31 +0900

    Enemy.hを編集

--- Enemy.h
+++ Enemy.h
@@ -1,1 +1,30 @@
 #pragma once
+#include "Novice.h"
+
+class Enemy {
+public:
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// ©ƒLƒƒƒ‰
+	/// XV
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
+
+private:
+
+
+
+};
+
+Enemy::Enemy() {}
+
+Enemy::~Enemy() {}
