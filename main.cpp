9b927bfa611ef4260b62a28ef4128c3a87d4a3b0
KoshidaManato <k022g0129@g.neec.ac.jp>
2023-09-09 23:36:46 +0900

    ゲームオーバー画面と3回勝負がついたらシーン移動の処理

--- main.cpp
+++ main.cpp
@@ -27,6 +27,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int curtainFlame = 0;
 	int curtainY = -1080;
 
+	//敵のフェーズの番号
+	int enemyPhaseNum = 0;
+
 #pragma region 自機の変数
 	const int Players = 5;
 	Player* player[Players] = {new Player, new Player, new Player, new Player, new Player};
@@ -84,7 +87,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Scene GameScene{
 	    0, // タイトル
 	    1, // ゲーム画面
-	    2, // シーン
+	    2, // ゲームクリア
+		3, //ゲームオーバー
 	};
 	Scene GamePhase{
 	    0, // 敵戦闘シーン
@@ -134,12 +138,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 						enemy[i]->Initialize();
 						player[i]->Initialize(); // Player初期化
 						Titleanime->Initialize();
+						playerHP = 3;
+						enemyHP = 3;
+						degreeH = 0;
 						PlayerFormation = 0;
 					}
 					curtainDown = true;
 						break;
-					}
 				}
+			}
 			if (curtainDown == true) {
 				    curtainY += 5;
 			}
@@ -154,6 +161,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 			case 1: // ゲーム中
 #pragma region シーン変更
+			//幕の処理
+			if (curtainFlame <= 60) {
+				    if (curtainY == 0) {
+					    curtainFlame++;
+				    }
+			} else if (curtainFlame > 60) {
+				    curtainUp = true;
+				    if (curtainUp == true) {
+					    curtainY -= 2;
+					    if (curtainY <= -1080) {
+							curtainY = -1080;
+							curtainUp == false;
+					    }
+					    curtainFlame = 0;
+				    }
+				    if (Novice::IsTriggerButton(0, kPadButton1)) {
+					    for (int i = 0; i < 5; i++) {
+							player[i]->Initialize();
+					    }
+				    }
+			}
 			if (curtainY >= 0) {
 				    curtainFlame++;
 			}
@@ -168,11 +196,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				    curtainUp = false;
 				    curtainFlame = 0;
 			}
-				if (Novice::IsTriggerButton(0, kPadButton1)) {
+			if (curtainY >= 0) {
+				    curtainFlame++;
+			}
+			if (curtainFlame >= 60) {
+				    curtainUp = true;
+			}
+			if (curtainUp == true) {
+				    curtainY -= 5;
+			}
+			if (curtainY <= -1080) {
+				    curtainY = -1080;
+				    curtainUp = false;
+				    curtainFlame = 0;
+			}
+				/*if (Novice::IsTriggerButton(0, kPadButton1)) {
 				   for (int i = 0; i < 5; i++) {
 				    player[i]->Initialize();
 				   }
-			    }
+			    }*/
 				if (GameScene.scene == 1) {
 					backGroundFlame++;
 					if (backGroundFlame >= 121) {
@@ -183,148 +225,169 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 					} else {
 						backGroundNum = 1;
 					}
-					if (degreeH < 750) {
-						// 敵戦闘とアイテム選択シーンの切り替え
-						if (GamePhase.phase == 0) {
-							/// 敵の生存フラグがfalseになったらアイテム選択フェーズに移動する////
+				    if (playerHP > 0) {
+					    if (degreeH < 750) {
+						    // 敵戦闘とアイテム選択シーンの切り替え
+						    if (GamePhase.phase == 0) {
+							    /// 敵と3回戦ったらアイテム選択フェーズに移動する////
 
-							if (keys[DIK_RETURN] && preKeys[DIK_RETURN] == 0) {
-								degreeH += 150;
-								GamePhase.phase = 1;
-							}
+							    if (enemyHP <= 0) {
+								    degreeH += 150;
+								    if (degreeH <= 300) {
+									    GamePhase.phase = 1;
+								    } else if (300 < degreeH < 750) {
+									    enemyHP = 3;
+								    }
+							    }
 
-							////////////////////////////////////////////////////////////////////
-						}
-						if (GamePhase.phase == 1) {
-							////////////////アイテムを選択するフェーズの処理////////////////////
+							    ////////////////////////////////////////////////////////////////////
+						    }
+						    if (GamePhase.phase == 1) {
+							    ////////////////アイテムを選択するフェーズの処理////////////////////
 
-							if (Novice::IsTriggerButton(0, kPadButton12) &&
-							    Novice::IsTriggerButton(0, kPadButton13) == 0 &&
-							    Novice::IsTriggerButton(0, kPadButton11) == 0) {
-								/// 自機のHPが1回復する処理
-								if (playerHP < 5) {
-									playerHP += 1;
-								}
-								degreeH += 150;
-								GamePhase.phase = 0;
-							}
-							if (Novice::IsTriggerButton(0, kPadButton13) &&
-							    Novice::IsTriggerButton(0, kPadButton12) == 0 &&
-							    Novice::IsTriggerButton(0, kPadButton11) == 0) {
-								// 次の入力待機時間が少し長くなる処理
+							    if (Novice::IsTriggerButton(0, kPadButton12) &&
+							        Novice::IsTriggerButton(0, kPadButton13) == 0 &&
+							        Novice::IsTriggerButton(0, kPadButton11) == 0) {
+								    /// 自機のHPが1回復する処理
+								    if (playerHP < 5) {
+									    playerHP += 1;
+								    }
+								    degreeH += 150;
+								    enemyHP = 3;
+								    GamePhase.phase = 0;
+							    }
+							    if (Novice::IsTriggerButton(0, kPadButton13) &&
+							        Novice::IsTriggerButton(0, kPadButton12) == 0 &&
+							        Novice::IsTriggerButton(0, kPadButton11) == 0) {
+								    // 次の入力待機時間が少し長くなる処理
 
-								degreeH += 150;
-								GamePhase.phase = 0;
-							}
-							if (Novice::IsTriggerButton(0, kPadButton11) &&
-							    Novice::IsTriggerButton(0, kPadButton13) == 0 &&
-							    Novice::IsTriggerButton(0, kPadButton12) == 0) {
-								// 目隠しを消す処理
+								    degreeH += 150;
+								    enemyHP = 3;
+								    GamePhase.phase = 0;
+							    }
+							    if (Novice::IsTriggerButton(0, kPadButton11) &&
+							        Novice::IsTriggerButton(0, kPadButton13) == 0 &&
+							        Novice::IsTriggerButton(0, kPadButton12) == 0) {
+								    // 目隠しを消す処理
 
-								degreeH += 150;
-								GamePhase.phase = 0;
-							}
+								    degreeH += 150;
+								    enemyHP = 3;
+								    GamePhase.phase = 0;
+							    }
 
-							////////////////////////////////////////////////////////////////////
-						}
-						if (Animation == true) {
-							for (int i = 0; i < Players; i++) {
-								player[i]->Update();
-							}
+							    ////////////////////////////////////////////////////////////////////
+						    }
+						    if (Animation == true) {
+							    for (int i = 0; i < Players; i++) {
+								    player[i]->Update();
+							    }
 
-							if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
-								GameScene.scene = 0;
-								Animation = false;
-								break;
-							}
-						}
+							    if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+								    GameScene.scene = 0;
+								    Animation = false;
+								    break;
+							    }
+						    }
 #pragma endregion
 
 #pragma region 自機の陣形変更
-						if (GamePhase.phase == 0) {
-							Animation = true;
-							if (Novice::IsTriggerButton(0, kPadButton2)) {//W
-								for (int i = 0; i < 5; i++) {
-									player[i]->MoveUpdate();
-									PlayerFormation = 1;
-								}
-							}
-							if (Novice::IsTriggerButton(0, kPadButton3)) {//×
-								for (int i = 0; i < 5; i++) {
-									player[i]->SecondMoveUpdate();
-									PlayerFormation = 2;
-								}
-							}
-							if (Novice::IsTriggerButton(0, kPadButton0)) {//〇
-								for (int i = 0; i < 5; i++) {
-									player[i]->ThirdMoveUpdate();
-									PlayerFormation = 3;
-								}
-							}
-							if (Novice::IsTriggerButton(0, kPadButton1)) {
-								for (int i = 0; i < 5; i++) {
-									player[i]->Initialize();
-									PlayerFormation = 0;
-								}
-							}
-						}
+						    if (GamePhase.phase == 0) {
+							    Animation = true;
+							    if (Novice::IsTriggerButton(0, kPadButton2)) { // W
+								    for (int i = 0; i < 5; i++) {
+									    player[i]->MoveUpdate();
+									    PlayerFormation = 1;
+								    }
+							    }
+							    if (Novice::IsTriggerButton(0, kPadButton3)) { // ×
+								    for (int i = 0; i < 5; i++) {
+									    player[i]->SecondMoveUpdate();
+									    PlayerFormation = 2;
+								    }
+							    }
+							    if (Novice::IsTriggerButton(0, kPadButton0)) { // 〇
+								    for (int i = 0; i < 5; i++) {
+									    player[i]->ThirdMoveUpdate();
+									    PlayerFormation = 3;
+								    }
+							    }
+							    if (Novice::IsTriggerButton(0, kPadButton1)) {
+								    for (int i = 0; i < 5; i++) {
+									    player[i]->Initialize();
+									    PlayerFormation = 0;
+								    }
+							    }
+						    }
 
 #pragma endregion
 
 #pragma region 敵の更新
-
-						for (int i = 0; i < Enemys; i++) {
-
-							enemy[i]->Update();
-						}
+						    if (GamePhase.phase == 0) {
+							    for (int i = 0; i < Enemys; i++) {
+								    enemy[i]->Update();
+							    }
+						    }
 #pragma endregion
 
 #pragma region 勝負
-
-						if (enemy[0]->GetTimever() <= 0) {
-							if (enemy[0]->Getformation() == 1) {//敵が逆への字で左ボタン
-								if (PlayerFormation == 1) {
-
-								}
-								else
-								{
-									playerHP -= 1;
-								}
-							}
-							if (enemy[0]->Getformation() == 2) {//敵がへの字で上ボタン
-								if (PlayerFormation == 2) {
-
-								}
-								else
-								{
-									playerHP -= 1;
-								}
-							}
-							if (enemy[0]->Getformation() == 3) {//敵がギザギザで右ボタン
-								if (PlayerFormation == 3) {
-
-								}
-								else
-								{
-									playerHP -= 1;
-								}
-							}
-						}
+						    if (GamePhase.phase == 0) {
+							    if (enemy[0]->GetTimever() <= 0) {
+								    if (enemy[0]->Getformation() == 1) { // 敵が逆への字で左ボタン
+									    if (PlayerFormation == 1) {
+										    enemyHP -= 1;
+									    } else {
+										    playerHP -= 1;
+										    enemyHP -= 1;
+									    }
+								    }
+								    if (enemy[0]->Getformation() == 2) { // 敵がへの字で上ボタン
+									    if (PlayerFormation == 2) {
+										    enemyHP -= 1;
+									    } else {
+										    playerHP -= 1;
+										    enemyHP -= 1;
+									    }
+								    }
+								    if (enemy[0]->Getformation() == 3) { // 敵がギザギザで右ボタン
+									    if (PlayerFormation == 3) {
+										    enemyHP -= 1;
+									    } else {
+										    playerHP -= 1;
+										    enemyHP -= 1;
+									    }
+								    }
+							    }
 
 #pragma endregion
-						break;
-					} else if (degreeH >= 750) {
-						GameScene.scene = 2;
+							    break;
+						    }
+					    } else if (degreeH >= 750) {
+						    GameScene.scene = 2;
+					    }
+				    } else {
+					    GameScene.scene = 3;
 					}
+				    
 				}
 			case 2: // ゲームクリア
 				if (GameScene.scene == 2) {
 					if (Novice::IsTriggerButton(0, kPadButton11)) {
 						GamePhase.phase = 0;
 						degreeH = 0;
+					    playerHP = 3;
 						GameScene.scene = 0;
 					}
 				}
+		    case 3: //ゲームオーバー
+			    if (GameScene.scene == 3) {
+				    if (Novice::IsTriggerButton(0, kPadButton11)) {
+					    GamePhase.phase = 0;
+					    degreeH = 0;
+						
+					    GameScene.scene = 0;
+				    }
+				}
+
 			}
 
 			///
@@ -348,12 +411,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				for (int i = 0; i < Players; i++) {
 					player[i]->Draw(); // Player描画
 				}
-
-				enemy[0]->Draw(); // Enemy描画
+			    if (GamePhase.phase == 0) {
+				    enemy[0]->Draw(); // Enemy描画
+				}
 
 			///進行度バーの描画
 			Novice::DrawBox(100, 915 - degreeH, degreeW, degreeH, 0.0f, RED, kFillModeSolid);
 			Novice::DrawBox(100, 165, degreeFrameW, degreeFlameH, 0.0f, BLACK, kFillModeWireFrame);
+
+			Novice::DrawBox(0, curtainY, 1920, 1080, 0.0f, GREEN, kFillModeSolid);
 
 			break;
 		case 2:
@@ -361,6 +427,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				Novice::ScreenPrintf(100, 100, "GAME CLEAR");
 			}
 			break;
+		case 3:
+			if (GameScene.scene == 3) {
+				Novice::ScreenPrintf(100, 100, "GAME OVER");
+			}
 		}
 
 #pragma region デバックコード
